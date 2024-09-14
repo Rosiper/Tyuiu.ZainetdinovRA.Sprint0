0cf465f92e992dfd4ec2f82af541d0dd7083f789
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-09-14 14:48:49 +0500

    Task1

---
+++ Tyuiu.ZainetdinovRA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZainetdinovRA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Ruslan";
+            int age = 18;
+            Console.WriteLine("Hello my name is "+name+" I'm "+age+" years old."); 
+            Console.ReadKey();
+        }
+    }
+}
