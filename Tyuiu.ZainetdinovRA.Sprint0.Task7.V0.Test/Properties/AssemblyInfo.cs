b56e304c603a388f78acc2c89fe21b0951e2e7bd
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-09-17 20:05:44 +0500

    Task7

---
+++ Tyuiu.ZainetdinovRA.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZainetdinovRA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZainetdinovRA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80729502-7fe1-4797-850a-80c77ca51e0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZainetdinovRA.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZainetdinovRA.Sprint0.Task7.V0.Lib;
+
+
+namespace Tyuiu.ZainetdinovRA.Sprint0.Task7.V0
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Title = "Спринт #0 | Выполнил: Zainetdinov R. A. | ИИПБ-24-2";
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("* Спринт #0                                                           *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                         *");
+            Console.WriteLine("* Задание #7                                                          *");
+            Console.WriteLine("* Вариант #0                                                          *");
+            Console.WriteLine("* Выполнил: Зайнетдинов Руслан Александрович | ИИПБ-24-2              *");
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                            *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения     *");
+            Console.WriteLine("* двух одинаковых массивов по длине                                   *");
+            Console.WriteLine("*                                                                     *");
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                    *");
+            Console.WriteLine("***********************************************************************");
+
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Values of the first array elements");
+            for (int i = 0; i < arrayNums1.Length; i++)
+            {
+                Console.Write(arrayNums1[i] + ", ");
+            }
+
+
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Values of the second array elements");
+            for (int i = 0;i < arrayNums2.Length; i++)
+            {
+                Console.Write(arrayNums2[i] + ", ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                          *");
+            Console.WriteLine("***********************************************************************");
+
+            if (arrayNums1.Length == arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+                Console.WriteLine("Sum of array elements is equal to:");
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    Console.Write(resultArray[i] + ", ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error! The initial arrays have different numbers of elements");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
